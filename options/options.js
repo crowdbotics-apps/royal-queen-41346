414c9f017fa1f0ae53379da57994c078cf33d543
Crowdbotics <team@crowdbotics.com>
2023-06-07 19:30:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-queen-41346",
+  url: "https://royal-queen-41346.botics.co",
+  api: "https://royal-queen-41346.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
